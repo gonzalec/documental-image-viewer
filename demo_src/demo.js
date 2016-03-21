5de716930a92dedcc014cb94e3747ccce1e5772d
César González <gonzalec@gmail.com>
2016-03-21 02:34:49 +0100

    chore(demo.js): adds info comment

--- demo_src/demo.js
+++ demo_src/demo.js
@@ -2,8 +2,8 @@ import './styles/demo.scss';
 import angular from 'angular';
 import DemoController from './demo.controller';
 
-// import './index.jade';
-// import '../../src/documental.image.viewer.directive.js';
+// import '../src/documental.image.viewer.directive.js';
+// a dependency to an entry point is not allowed.
 
 export default angular.module('demoApp', ['directives.docivImageViewer'])
     .controller('DemoController', DemoController)
